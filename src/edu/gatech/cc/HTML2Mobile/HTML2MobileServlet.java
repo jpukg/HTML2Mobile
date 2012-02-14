28850e861a099e95a05f15643a515f6e5beef85f
Andrew Liu <aliuy8@gmail.com>
2012-02-14 13:58:55 -0500

    The rest of the dummy XML/XSL code... I am a GIT noob.

--- src/edu/gatech/cc/HTML2Mobile/HTML2MobileServlet.java
+++ src/edu/gatech/cc/HTML2Mobile/HTML2MobileServlet.java
@@ -1,13 +1,41 @@
 package edu.gatech.cc.HTML2Mobile;
 
+import java.io.File;
 import java.io.IOException;
-import javax.servlet.http.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 @SuppressWarnings("serial")
 public class HTML2MobileServlet extends HttpServlet {
+	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+	throws IOException {
+		// Set up Dummy XML
+		File xmlFile = new File("test.xml");
+		Source xmlSource = new StreamSource(xmlFile);
+
+		// Set up Dummy XSL
+		File xsltFile = new File("test.xsl");
+		Source xsltSource = new StreamSource(xsltFile);
+
+		// Apply XSL Transformation to XML
+		TransformerFactory transFact = TransformerFactory.newInstance();
+		try {
+			Transformer trans = transFact.newTransformer(xsltSource);
+			trans.transform(xmlSource, new StreamResult(resp.getWriter()));
+		} catch (TransformerConfigurationException e) {
+			resp.getWriter().println("Error: Unable to read XSLT Source");
+		} catch (TransformerException e) {
+			resp.getWriter().println("Error: Unable to read XML Source");
+		}
 	}
 }
