6370c467cf92ada06dfdf9668e6601f70ae3e1f3
Jake Cobb <jake.cobb@gatech.edu>
2012-02-20 18:31:22 -0500

    Abstract servlet base for document processing.

---
+++ src/main/java/edu/gatech/cc/HTML2Mobile/JSoupServlet.java
@@ -0,0 +1,54 @@
+package edu.gatech.cc.HTML2Mobile;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public abstract class JSoupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// read requested URL
+		String urlParam = req.getParameter("url");
+		if( urlParam == null ) {
+			throw new ServletException("No URL");
+		}
+		URL url = null;
+		try {
+			url = new URL(urlParam);
+		} catch( MalformedURLException e ) {
+			throw new ServletException("Cannot parse: " + urlParam, e);
+		}
+
+		/* Jersey Reference
+		// fetch the URL
+		Client client = Client.create();
+		WebResource res = client.resource(urlParam);
+		String contents = res.get(String.class);
+		// parse the document
+		Document doc = Jsoup.parse(contents); */
+
+		Document doc = Jsoup.connect(url.toString()).get();
+
+		String result = this.process(doc, req);
+
+		PrintWriter writer = resp.getWriter();
+		writer.write("<html><body><pre>");
+		writer.write(result.replace("<", "&lt;").replace(">", "&gt;"));
+		writer.write("</pre></body></html>");
+	}
+
+	protected abstract String process(Document doc, HttpServletRequest req)
+			throws ServletException, IOException;
+}
