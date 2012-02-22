1069b561cdb0227412b3394248f9d40226610bfa
David Esposito <desposito6@gatech.edu>
2012-02-22 18:18:23 -0500

    Converted FormParserServlet

--- src/main/java/edu/gatech/cc/HTML2Mobile/Parser/FormParserServlet.java
+++ src/main/java/edu/gatech/cc/HTML2Mobile/Parser/FormParserServlet.java
@@ -1,38 +1,31 @@
 package edu.gatech.cc.HTML2Mobile.Parser;
 
 import java.io.IOException;
-import java.net.URL;
 
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class FormParserServlet extends HttpServlet {
+import edu.gatech.cc.HTML2Mobile.JSoupServlet;
+
+public class FormParserServlet extends JSoupServlet {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		String url = req.getParameter("q");
-		if (url == null) {
-			resp.getWriter().print("<div style='color:red;fontweight:bold'>");
-			resp.getWriter().print("<center>Please  provide a valid url");
-			resp.getWriter().println("</center></div>");
-		} else {
-			Document doc = Jsoup.parse(new URL(url), 1000);
-			Elements forms = doc.select("form");
-			for (Element e : forms) {
-				FormObj fobj = new FormObj(e);
-				resp.getWriter().println(fobj.toHtml2Mobile_2());
-			}
+	protected String process(Document doc, HttpServletRequest req)
+			throws ServletException, IOException {
+		StringBuilder sb = new StringBuilder();
+		Elements forms = doc.select("form");
+		for (Element e : forms) {
+			FormObj fobj = new FormObj(e);
+			sb.append(fobj.toHtml2Mobile_2()).append("\n");
 		}
+		return sb.toString();
 	}
 }
