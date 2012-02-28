ade6319cc039c1756c917a4f82f80b780628b5b0
Abhishek Jain <abhishekjain@gatech.edu>
2012-02-28 15:49:24 -0500

    added IFRAMEservlet

---
+++ src/main/java/edu/gatech/cc/HTML2Mobile/Parser/BaseGroup.java
@@ -0,0 +1,55 @@
+package edu.gatech.cc.HTML2Mobile.Parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Element;
+
+public class BaseGroup {
+
+	private List<Element> elements;
+
+	private String outsideTag = "outside";
+	private String insideTag = "inside";
+
+
+	public BaseGroup(String in,String out){
+		elements = new ArrayList<Element>();
+		outsideTag = out;
+		insideTag = in;
+	}
+
+	public void addElement(Element e){
+		elements.add(e);
+	}
+
+
+	@Override
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("\t<" + outsideTag + ">\n");
+		for (Element a : elements) {
+
+			sb.append("\t\t<"+insideTag+">\n");
+
+			sb.append("\t\t\t<text>"+a.ownText()+"</text>\n");
+			for(Attribute attrib : a.attributes()) {
+				if(!attrib.getValue().isEmpty()) {
+					sb.append("\t\t\t<" + attrib.getKey() + ">" + attrib.getValue() + "</" + attrib.getKey() + ">\n");
+				}
+			}
+
+			sb.append("\t\t</"+insideTag+">\n");
+			//
+		}
+		sb.append("\t</" + outsideTag + ">\n");
+
+		return sb.toString();
+
+	}
+
+
+
+}
--- src/main/java/edu/gatech/cc/HTML2Mobile/Parser/IFrameServlet.java
+++ src/main/java/edu/gatech/cc/HTML2Mobile/Parser/IFrameServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import edu.gatech.cc.HTML2Mobile.JSoupServlet;
 
@@ -16,10 +18,13 @@ public class IFrameServlet extends JSoupServlet {
 	@Override
 	public String process(Document doc, HttpServletRequest req)
 			throws ServletException, IOException {
-		StringBuilder toRet = new StringBuilder("<iframe>\n");
 
-		toRet.append("</iframe>\n");
-		return toRet.toString();
+		Elements eles = doc.select("iframe");
+		BaseGroup group = new BaseGroup("iframe","iframegroup");
+		for (Element a : eles) {
+			group.addElement(a);
+		}
+		return group.toString();
 	}
 
 }
