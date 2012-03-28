89933c2b8e922aa960c1c951522160708b97d5e9
Jake Cobb <jake.cobb@gatech.edu>
2012-03-28 17:46:51 -0400

    Transformer skeleton.

---
+++ src/main/java/edu/gatech/cc/HTML2Mobile/transform/TransformException.java
@@ -0,0 +1,24 @@
+package edu.gatech.cc.HTML2Mobile.transform;
+
+public class TransformException extends RuntimeException {
+
+	public TransformException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public TransformException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TransformException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TransformException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
